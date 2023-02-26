53d7ad6866db8e651b948da629bbc455cc879204
Santiago Bustos <dasapibu@gmail.com>
2023-02-26 13:54:42 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Tarea2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelCourse = new System.Windows.Forms.Label();
+            this.LabelName = new System.Windows.Forms.Label();
+            this.LabelAcceso = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LabelCourse
+            // 
+            this.LabelCourse.AutoSize = true;
+            this.LabelCourse.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelCourse.Location = new System.Drawing.Point(139, 30);
+            this.LabelCourse.Name = "LabelCourse";
+            this.LabelCourse.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.LabelCourse.Size = new System.Drawing.Size(201, 24);
+            this.LabelCourse.TabIndex = 1;
+            this.LabelCourse.Text = "Estructuras de Datos";
+            this.LabelCourse.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LabelCourse.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LabelName
+            // 
+            this.LabelName.AutoSize = true;
+            this.LabelName.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelName.Location = new System.Drawing.Point(105, 70);
+            this.LabelName.Name = "LabelName";
+            this.LabelName.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.LabelName.Size = new System.Drawing.Size(269, 20);
+            this.LabelName.TabIndex = 2;
+            this.LabelName.Text = "HOLMAN ESTIH FONSECA ROJAS";
+            this.LabelName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LabelName.Click += new System.EventHandler(this.LabelName_Click);
+            // 
+            // LabelAcceso
+            // 
+            this.LabelAcceso.AutoSize = true;
+            this.LabelAcceso.Font = new System.Drawing.Font("Microsoft JhengHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelAcceso.Location = new System.Drawing.Point(170, 110);
+            this.LabelAcceso.Name = "LabelAcceso";
+            this.LabelAcceso.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.LabelAcceso.Size = new System.Drawing.Size(139, 19);
+            this.LabelAcceso.TabIndex = 3;
+            this.LabelAcceso.Text = "Acceso al Sistema";
+            this.LabelAcceso.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft JhengHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(120, 162);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelPassword.Size = new System.Drawing.Size(91, 19);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Contraseña";
+            this.labelPassword.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(211, 160);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(145, 24);
+            this.tbPassword.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(180, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(460, 297);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.LabelAcceso);
+            this.Controls.Add(this.LabelName);
+            this.Controls.Add(this.LabelCourse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.96D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelCourse;
+        private System.Windows.Forms.Label LabelName;
+        private System.Windows.Forms.Label LabelAcceso;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
